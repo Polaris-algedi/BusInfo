abe7469c3d6f5bbf9c40800fe63e46e0ec1f8d6e
Polaris-algedi <karchalmohammed@gmail.com>
2023-11-23 00:36:11 +0100

    Import newly created classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,10 +6,14 @@ connection.
 import os
 from models.base_model import Base
 from models.user import User
+from models.route import Route
+from models.feedback import Feedback
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 name2class = {
-    'User': User
+    'User': User,
+    'Route': Route,
+    'Feedback': Feedback
 }
 
 
