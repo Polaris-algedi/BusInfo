cfde61fff5c6a353fceb5d2b8753fc7d9f47d8a8
Polaris-algedi <karchalmohammed@gmail.com>
2024-02-05 00:21:24 +0100

    Updated schedule model columns, replaced deltatime with time

--- models/schedule.py
+++ models/schedule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a Schedule class."""
-from datetime import time, timedelta
+from datetime import time
 from models.base_model import BaseModel, Base
 from models.route import Route
 from sqlalchemy import String
@@ -16,7 +16,7 @@ class Schedule(BaseModel, Base):
 
     first_departure: Mapped[time] = mapped_column(nullable=False, default=time(hour=6))
     last_departure: Mapped[time] = mapped_column(nullable=False, default=time(hour=21))
-    duration: Mapped[timedelta] = mapped_column(nullable=False, default=timedelta(hours=1))
-    bus_frequency: Mapped[timedelta] = mapped_column(nullable=False, default=timedelta(minutes=20))
+    duration: Mapped[time] = mapped_column(nullable=False, default=time(hour=1))
+    bus_frequency: Mapped[time] = mapped_column(nullable=False, default=time(minute=20))
     day_of_week: Mapped[str] = mapped_column(String(60), nullable=True)
     routes: Mapped[List['Route']] = relationship(back_populates='schedule', cascade="all, delete-orphan")
